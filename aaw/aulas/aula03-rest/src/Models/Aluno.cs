598ae448b6998a29d479c6ae3d1c9dc99abbe9d8
Matheus Braga <153444119+BragaTy@users.noreply.github.com>
2026-08-21 08:15:08 -0300

    att pastas

--- aaw/aulas/aula03-rest/src/Models/Aluno.cs
+++
@@ -1,9 +0,0 @@
-namespace EscolaApi.Models;
-
-public class Aluno
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Curso { get; set; } = string.Empty;
-    public ICollection<Matricula> Matriculas { get; set; } = new List<Matricula>();
-}
